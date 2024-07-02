fd71ffed084f80c74a0a340c5900e84f44fa2295
lazing-wizard <119129322+lazing-wizard@users.noreply.github.com>
2024-07-02 23:58:40 +0300

    Add files via upload

---
+++ include/sample_lib/head.hpp
@@ -0,0 +1,11 @@
+#ifndef SAMPLE_LIB_HPP
+#define SAMPLE_LIB_HPP
+
+namespace sample_lib {
+    
+int getnum();
+void setnum(int n);
+
+}
+
+#endif //SAMPLE_LIB_HPP
---
+++ source/src.cpp
@@ -0,0 +1,15 @@
+#include "sample_lib/head.hpp"
+
+namespace sample_lib {
+
+int number = 7212;
+
+int getnum() {
+    return number;
+}
+
+void setnum(int n) {
+    number = n;
+}
+    
+}
